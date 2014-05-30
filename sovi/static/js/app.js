f543e06e5fb2aa446365c70e1996826e7e89f973
Mahdi Hussein <m@hdihusse.in>
2014-05-29 22:33:07 -0400

    Add Script Editing & Multiple Views
    - Serious Refactor
    - Add Angular with Multiple views
    - Prepare Views
    - Add basic Script edit (Ace)
    - Complete all boilerplate

--- sovi/static/js/app.js
+++ sovi/static/js/app.js
@@ -1,8 +1,57 @@
 'use strict';
 
 var sovi = angular.module('sovi', [
-    'soviControllers'
+    'ngRoute',
+    'soviControllers',
 ]);
 
-/* Controller */
+/* Router */
+sovi.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider.
+      when('/', {
+        title: 'Home',
+        templateUrl: '/static/partials/home.html',
+        controller: 'AdminHome'
+      }).
+      when('/script', {
+        title: 'Script Editor',
+        templateUrl: '/static/partials/editor.html',
+        controller: 'AdminScriptEditor'
+      }).
+      when('/users/me/preferences', {
+        title: 'My Preferences',
+        templateUrl: '/static/partials/preferences.html',
+        controller: 'AdminPreferences'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+sovi.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
+
+/* Controllers */
 var soviControllers = angular.module('soviControllers', []);
+
+soviControllers.controller('AdminHome', ['$scope', '$http', 
+  function($scope, $http) {
+  }]);
+
+soviControllers.controller('AdminScriptEditor', ['$scope', '$http', 
+  function($scope, $http) {
+    // Init editor with defaults...
+    $scope.editor = ace.edit("editor");
+    $scope.editor.setTheme("ace/theme/solarized_dark");
+    $scope.editor.getSession().setMode("ace/mode/python");
+  }]);
+
+soviControllers.controller('AdminPreferences', ['$scope', '$http', 
+  function($scope, $http) {
+  }]);
+
